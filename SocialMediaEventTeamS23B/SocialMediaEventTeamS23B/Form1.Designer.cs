21895e0d930a6d056dc60255e77584f315b09151
Rick <rick-boy1@live.nl>
2015-03-27 10:19:32 +0100

    SocialMediaSharing form creation

--- SocialMediaEventTeamS23B/SocialMediaEventTeamS23B/Form1.Designer.cs
+++ SocialMediaEventTeamS23B/SocialMediaEventTeamS23B/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SocialMediaEventTeamS23B
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,163 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btLogin = new System.Windows.Forms.Button();
+            this.lblLoggedIn = new System.Windows.Forms.Label();
+            this.btnAdmin = new System.Windows.Forms.Button();
+            this.btnAccesControl = new System.Windows.Forms.Button();
+            this.btnSocialMediaSharing = new System.Windows.Forms.Button();
+            this.btnMaterial = new System.Windows.Forms.Button();
+            this.btnReserving = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(275, 169);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(150, 20);
+            this.tbUsername.TabIndex = 0;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(275, 208);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(150, 20);
+            this.tbPassword.TabIndex = 1;
+            this.tbPassword.UseSystemPasswordChar = true;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(320, 153);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(55, 13);
+            this.lblUsername.TabIndex = 2;
+            this.lblUsername.Text = "Username";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(322, 192);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 3;
+            this.lblPassword.Text = "Password";
+            // 
+            // btLogin
+            // 
+            this.btLogin.Location = new System.Drawing.Point(275, 234);
+            this.btLogin.Name = "btLogin";
+            this.btLogin.Size = new System.Drawing.Size(150, 23);
+            this.btLogin.TabIndex = 4;
+            this.btLogin.Text = "Login";
+            this.btLogin.UseVisualStyleBackColor = true;
+            // 
+            // lblLoggedIn
+            // 
+            this.lblLoggedIn.AutoSize = true;
+            this.lblLoggedIn.Location = new System.Drawing.Point(12, 9);
+            this.lblLoggedIn.Name = "lblLoggedIn";
+            this.lblLoggedIn.Size = new System.Drawing.Size(70, 13);
+            this.lblLoggedIn.TabIndex = 5;
+            this.lblLoggedIn.Text = "Not logged in";
+            // 
+            // btnAdmin
+            // 
+            this.btnAdmin.Enabled = false;
+            this.btnAdmin.Location = new System.Drawing.Point(12, 289);
+            this.btnAdmin.Name = "btnAdmin";
+            this.btnAdmin.Size = new System.Drawing.Size(120, 60);
+            this.btnAdmin.TabIndex = 6;
+            this.btnAdmin.Text = "Administration";
+            this.btnAdmin.UseVisualStyleBackColor = true;
+            this.btnAdmin.Visible = false;
+            // 
+            // btnAccesControl
+            // 
+            this.btnAccesControl.Enabled = false;
+            this.btnAccesControl.Location = new System.Drawing.Point(138, 289);
+            this.btnAccesControl.Name = "btnAccesControl";
+            this.btnAccesControl.Size = new System.Drawing.Size(120, 60);
+            this.btnAccesControl.TabIndex = 7;
+            this.btnAccesControl.Text = "Toegangs Controlle";
+            this.btnAccesControl.UseVisualStyleBackColor = true;
+            this.btnAccesControl.Visible = false;
+            // 
+            // btnSocialMediaSharing
+            // 
+            this.btnSocialMediaSharing.Enabled = false;
+            this.btnSocialMediaSharing.Location = new System.Drawing.Point(264, 289);
+            this.btnSocialMediaSharing.Name = "btnSocialMediaSharing";
+            this.btnSocialMediaSharing.Size = new System.Drawing.Size(156, 60);
+            this.btnSocialMediaSharing.TabIndex = 8;
+            this.btnSocialMediaSharing.Text = "Social Media Sharing";
+            this.btnSocialMediaSharing.UseVisualStyleBackColor = true;
+            this.btnSocialMediaSharing.Visible = false;
+            // 
+            // btnMaterial
+            // 
+            this.btnMaterial.Enabled = false;
+            this.btnMaterial.Location = new System.Drawing.Point(426, 289);
+            this.btnMaterial.Name = "btnMaterial";
+            this.btnMaterial.Size = new System.Drawing.Size(120, 60);
+            this.btnMaterial.TabIndex = 9;
+            this.btnMaterial.Text = "Materiaal uitleen";
+            this.btnMaterial.UseVisualStyleBackColor = true;
+            this.btnMaterial.Visible = false;
+            // 
+            // btnReserving
+            // 
+            this.btnReserving.Enabled = false;
+            this.btnReserving.Location = new System.Drawing.Point(552, 289);
+            this.btnReserving.Name = "btnReserving";
+            this.btnReserving.Size = new System.Drawing.Size(120, 60);
+            this.btnReserving.TabIndex = 10;
+            this.btnReserving.Text = "Reservering";
+            this.btnReserving.UseVisualStyleBackColor = true;
+            this.btnReserving.Visible = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(684, 361);
+            this.Controls.Add(this.btnReserving);
+            this.Controls.Add(this.btnMaterial);
+            this.Controls.Add(this.btnSocialMediaSharing);
+            this.Controls.Add(this.btnAccesControl);
+            this.Controls.Add(this.btnAdmin);
+            this.Controls.Add(this.lblLoggedIn);
+            this.Controls.Add(this.btLogin);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUsername);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Social Media Event - Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btLogin;
+        private System.Windows.Forms.Label lblLoggedIn;
+        private System.Windows.Forms.Button btnAdmin;
+        private System.Windows.Forms.Button btnAccesControl;
+        private System.Windows.Forms.Button btnSocialMediaSharing;
+        private System.Windows.Forms.Button btnMaterial;
+        private System.Windows.Forms.Button btnReserving;
     }
 }
 
