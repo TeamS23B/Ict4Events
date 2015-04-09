88dbd4c6808e647394a90669ddd16924ba211d35
Skaj7 <kaj753@hotmail.com>
2015-04-09 11:42:49 +0200

    start reervation

---
+++ SocialMediaEventTeamS23B/ApplicationLayer/Reservation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatabaseConnection.Types;
+
+namespace ApplicationLayer
+{
+    public class Reservation
+    {
+        public List<Visitor> GroupMembers { get; private set; }
+
+        public Reservation(List<Visitor> groupMember, Visitor groupLeader, List<MapLocation> Location)
+        {
+            groupMember = null;
+            Location = null;
+
+        }
+
+    }
+}
