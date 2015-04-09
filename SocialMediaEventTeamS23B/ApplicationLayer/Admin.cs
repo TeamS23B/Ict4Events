49c6d582dbeaf5a3a0eff6db35d401bbc747d068
Skaj7 <kaj753@hotmail.com>
2015-04-09 12:13:37 +0200

    start van applicationlayer Admin

---
+++ SocialMediaEventTeamS23B/ApplicationLayer/Admin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatabaseConnection.Types;
+
+namespace ApplicationLayer
+{
+    class Admin
+    {
+        public List<Material> matrials { get; private set; }
+
+        public List<Event> events { get; private set; }
+        public Admin()
+        {
+
+        }
+        public void AddEvent()
+        {
+
+        }
+        public void AddMaterial()
+        {
+
+        }
+        public void GetLocation()
+        {
+
+        }
+        public void GetMaterial()
+        {
+
+        }
+        public void EditMaterial()
+        {
+
+        }
+        public void EditFlaggingControl()
+        {
+
+        }
+    }
+}
