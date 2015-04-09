b3bad201c2dde25212774a1c5fb8df99425a0ea9
Skaj7 <kaj753@hotmail.com>
2015-04-09 11:52:23 +0200

    reservation done

---
+++ SocialMediaEventTeamS23B/ApplicationLayer/Exceptions/NullException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationLayer.Exceptions
+{
+    class NullException:Exception
+    {
+        public NullException(String message): base(message)
+        {
+
+        }
+    }
+}
