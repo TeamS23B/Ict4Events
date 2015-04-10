29cdefb6d4a0705fac72a64b359b97eecd24a8ce
Qunfong <Qunfong@live.nl>
2015-04-10 10:01:51 +0200

    applicationlayer:Login

---
+++ SocialMediaEventTeamS23B/ApplicationLayer/Login.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationLayer.Exceptions;
+using DatabaseConnection.Types;
+
+namespace ApplicationLayer
+{
+    public class Login
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public Login(string Username, string Password)
+        {
+            if(Username =="" || Password =="")
+            {
+                throw new NullException("Voer correcte gebruikersnaam en wachtwoord in");
+            }
+            else
+            {
+                this.Username = Username;
+                this.Password = Password;
+            }
+        }
+    }
+}
