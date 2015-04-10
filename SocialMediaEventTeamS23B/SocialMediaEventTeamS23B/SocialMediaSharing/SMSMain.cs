7cdefd6cc4822460461cafe6fbd931ae09f5f130
Rick <rick-boy1@live.nl>
2015-04-10 14:04:20 +0200

    Continued SMS Main

--- SocialMediaEventTeamS23B/SocialMediaEventTeamS23B/SocialMediaSharing/SMSMain.cs
+++ SocialMediaEventTeamS23B/SocialMediaEventTeamS23B/SocialMediaSharing/SMSMain.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DatabaseConnection.Types;
+using ApplicationLayer;
 
 namespace SocialMediaEventTeamS23B.SocialMediaSharing
 {
@@ -19,31 +20,73 @@ namespace SocialMediaEventTeamS23B.SocialMediaSharing
             this.user = user;
             lblUsername.Text = user.Username;
             
-            Sms = new object();
+            Sms = new SocialMediaSharing(user.Username);
             messages=new List<SMSMessageMain>();
+            trendingMessages=new List<SMSMesageMainMedia>();
 
             tmrRefresh.Enabled = true;
         }
 
         private Visitor user;
-        private object Sms;
+        private SocialMediaSharing Sms;
 
-        private List<SMSMessageMain> messages; 
-
-        private int lastIndex;
+        private List<SMSMessageMain> messages;
+        private List<SMSMessageMain> trendingMessages;
+        private List<SMSMessageMain> myMessages;
 
         private void tmrRefresh_Tick(object sender, EventArgs e)
         {
             //checking for new content
-            var newContent = Sms.CheckContent(lastIndex);
-            foreach (Post content in newContent)
+            if (panel2.VerticalScroll.Value > panel2.VerticalScroll.Maximum - 300)
             {
-                var obj = new SMSMessageMain(content);
-                obj.Width = panel2.Width - 24;
+                var newContent = Sms.CheckContent(messages.Last());
+                foreach (Post content in newContent)
+                {
+                    var obj = new SMSMessageMain(content);
+                    obj.Width = panel2.Width - 24;
+                    obj.Left = 12;
+                    var lastMessage = messages.LastOrDefault();
+                    if (lastMessage == null)
+                        obj.Top = 12;
+                    else
+                        obj.Top = lastMessage.Top + lastMessage.Height + 12;
+                    panel2.Controls.Add(obj);
+                }
+
+            }
+
+            trendingMessages.ForEach(message=>panel3.Controls.Remove(message));
+            trendingMessages.Clear();
+
+            var topPosts = Sms.GetTopMessages();
+            foreach (Post topPost in topPosts)
+            {
+                var obj = new SMSMessageMain(topPost);
+                obj.Width = panel3.Width - 24;
                 obj.Left = 12;
-                var lastMessage = messages.Last();
-                obj.Top = lastMessage.Top + lastMessage.Height + 12;
-                panel2.Controls.Add(obj);
+                var lastMessage = trendingMessages.LastOrDefault();
+                if (lastMessage == null)
+                    obj.Top = 12;
+                else
+                    obj.Top = lastMessage.Top + lastMessage.Height + 12;
+                panel3.Controls.Add(obj);
+            }
+
+            myMessages.ForEach(message=>panel1.Controls.Remove(message));
+            myMessages.Clear();
+
+            var myPosts = Sms.GetMyPosts(user.Username);
+            foreach (Post myPost in myPosts)
+            {
+                var obj = new SMSMessageMain(myPost);
+                obj.Width = panel1.Width - 24;
+                obj.Left = 12;
+                var lastMessage = myMessages.LastOrDefault();
+                if (lastMessage == null)
+                    obj.Top = 12;
+                else
+                    obj.Top = lastMessage.Top + lastMessage.Height + 12;
+                panel1.Controls.Add(obj);
             }
         }
 
