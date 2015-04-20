8178f5ba133f5e18538bb46a3980fa6b0efcb8df
Jimbolul <krijgdekak@gmail.com>
2015-04-20 09:14:58 +0200

    Minor fixes

--- SocialMediaEventTeamS23B/SocialMediaEventTeamS23B/Reservation/ReservationLocation.Designer.cs
+++ SocialMediaEventTeamS23B/SocialMediaEventTeamS23B/Reservation/ReservationLocation.Designer.cs
@@ -34,37 +34,34 @@
             this.btnReservationLocationPrevious = new System.Windows.Forms.Button();
             this.btnReservationLocationDivideGroup = new System.Windows.Forms.Button();
             this.btnReservationLocationNext = new System.Windows.Forms.Button();
-            this.lblReservationLocationPersons = new System.Windows.Forms.Label();
-            this.lblReservationLocationAmountOfPeople = new System.Windows.Forms.Label();
-            this.lblReservationLocationStillToDivide = new System.Windows.Forms.Label();
-            this.lblReservationLocationStillToDivideAmount = new System.Windows.Forms.Label();
-            this.lblReservationLocationTotalAmount = new System.Windows.Forms.Label();
-            this.lblReservationLocationTotalAmountPrice = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblReservationLocationDetails
             // 
             this.lblReservationLocationDetails.AutoSize = true;
-            this.lblReservationLocationDetails.Location = new System.Drawing.Point(1118, 218);
+            this.lblReservationLocationDetails.Location = new System.Drawing.Point(1496, 24);
+            this.lblReservationLocationDetails.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblReservationLocationDetails.Name = "lblReservationLocationDetails";
-            this.lblReservationLocationDetails.Size = new System.Drawing.Size(42, 13);
+            this.lblReservationLocationDetails.Size = new System.Drawing.Size(55, 17);
             this.lblReservationLocationDetails.TabIndex = 1;
             this.lblReservationLocationDetails.Text = "Details:";
             // 
             // lblReservationLocationBungalow
             // 
             this.lblReservationLocationBungalow.AutoSize = true;
-            this.lblReservationLocationBungalow.Location = new System.Drawing.Point(1118, 236);
+            this.lblReservationLocationBungalow.Location = new System.Drawing.Point(1496, 46);
+            this.lblReservationLocationBungalow.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblReservationLocationBungalow.Name = "lblReservationLocationBungalow";
-            this.lblReservationLocationBungalow.Size = new System.Drawing.Size(116, 13);
+            this.lblReservationLocationBungalow.Size = new System.Drawing.Size(154, 17);
             this.lblReservationLocationBungalow.TabIndex = 2;
             this.lblReservationLocationBungalow.Text = "Bungalow - 8 personen";
             // 
             // btnReservationLocationPrevious
             // 
-            this.btnReservationLocationPrevious.Location = new System.Drawing.Point(1118, 831);
+            this.btnReservationLocationPrevious.Location = new System.Drawing.Point(1499, 118);
+            this.btnReservationLocationPrevious.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnReservationLocationPrevious.Name = "btnReservationLocationPrevious";
-            this.btnReservationLocationPrevious.Size = new System.Drawing.Size(75, 23);
+            this.btnReservationLocationPrevious.Size = new System.Drawing.Size(100, 28);
             this.btnReservationLocationPrevious.TabIndex = 3;
             this.btnReservationLocationPrevious.Text = "Stap terug";
             this.btnReservationLocationPrevious.UseVisualStyleBackColor = true;
@@ -72,9 +69,10 @@
             // 
             // btnReservationLocationDivideGroup
             // 
-            this.btnReservationLocationDivideGroup.Location = new System.Drawing.Point(1118, 265);
+            this.btnReservationLocationDivideGroup.Location = new System.Drawing.Point(1496, 82);
+            this.btnReservationLocationDivideGroup.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnReservationLocationDivideGroup.Name = "btnReservationLocationDivideGroup";
-            this.btnReservationLocationDivideGroup.Size = new System.Drawing.Size(91, 23);
+            this.btnReservationLocationDivideGroup.Size = new System.Drawing.Size(121, 28);
             this.btnReservationLocationDivideGroup.TabIndex = 4;
             this.btnReservationLocationDivideGroup.Text = "Deel groep in";
             this.btnReservationLocationDivideGroup.UseVisualStyleBackColor = true;
@@ -82,85 +80,27 @@
             // 
             // btnReservationLocationNext
             // 
-            this.btnReservationLocationNext.Location = new System.Drawing.Point(1199, 831);
+            this.btnReservationLocationNext.Location = new System.Drawing.Point(1607, 118);
+            this.btnReservationLocationNext.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnReservationLocationNext.Name = "btnReservationLocationNext";
-            this.btnReservationLocationNext.Size = new System.Drawing.Size(75, 23);
+            this.btnReservationLocationNext.Size = new System.Drawing.Size(100, 28);
             this.btnReservationLocationNext.TabIndex = 5;
             this.btnReservationLocationNext.Text = "Volgende stap";
             this.btnReservationLocationNext.UseVisualStyleBackColor = true;
             this.btnReservationLocationNext.Click += new System.EventHandler(this.btnReservationLocationNext_Click);
             // 
-            // lblReservationLocationPersons
-            // 
-            this.lblReservationLocationPersons.AutoSize = true;
-            this.lblReservationLocationPersons.Location = new System.Drawing.Point(1133, 13);
-            this.lblReservationLocationPersons.Name = "lblReservationLocationPersons";
-            this.lblReservationLocationPersons.Size = new System.Drawing.Size(87, 13);
-            this.lblReservationLocationPersons.TabIndex = 6;
-            this.lblReservationLocationPersons.Text = "Aantal personen:";
-            // 
-            // lblReservationLocationAmountOfPeople
-            // 
-            this.lblReservationLocationAmountOfPeople.AutoSize = true;
-            this.lblReservationLocationAmountOfPeople.Location = new System.Drawing.Point(1158, 37);
-            this.lblReservationLocationAmountOfPeople.Name = "lblReservationLocationAmountOfPeople";
-            this.lblReservationLocationAmountOfPeople.Size = new System.Drawing.Size(13, 13);
-            this.lblReservationLocationAmountOfPeople.TabIndex = 7;
-            this.lblReservationLocationAmountOfPeople.Text = "4";
-            // 
-            // lblReservationLocationStillToDivide
-            // 
-            this.lblReservationLocationStillToDivide.AutoSize = true;
-            this.lblReservationLocationStillToDivide.Location = new System.Drawing.Point(1142, 66);
-            this.lblReservationLocationStillToDivide.Name = "lblReservationLocationStillToDivide";
-            this.lblReservationLocationStillToDivide.Size = new System.Drawing.Size(79, 13);
-            this.lblReservationLocationStillToDivide.TabIndex = 8;
-            this.lblReservationLocationStillToDivide.Text = "Nog in te delen";
-            // 
-            // lblReservationLocationStillToDivideAmount
-            // 
-            this.lblReservationLocationStillToDivideAmount.AutoSize = true;
-            this.lblReservationLocationStillToDivideAmount.Location = new System.Drawing.Point(1158, 94);
-            this.lblReservationLocationStillToDivideAmount.Name = "lblReservationLocationStillToDivideAmount";
-            this.lblReservationLocationStillToDivideAmount.Size = new System.Drawing.Size(13, 13);
-            this.lblReservationLocationStillToDivideAmount.TabIndex = 9;
-            this.lblReservationLocationStillToDivideAmount.Text = "2";
-            // 
-            // lblReservationLocationTotalAmount
-            // 
-            this.lblReservationLocationTotalAmount.AutoSize = true;
-            this.lblReservationLocationTotalAmount.Location = new System.Drawing.Point(1142, 123);
-            this.lblReservationLocationTotalAmount.Name = "lblReservationLocationTotalAmount";
-            this.lblReservationLocationTotalAmount.Size = new System.Drawing.Size(70, 13);
-            this.lblReservationLocationTotalAmount.TabIndex = 10;
-            this.lblReservationLocationTotalAmount.Text = "Totaalbedrag";
-            // 
-            // lblReservationLocationTotalAmountPrice
-            // 
-            this.lblReservationLocationTotalAmountPrice.AutoSize = true;
-            this.lblReservationLocationTotalAmountPrice.Location = new System.Drawing.Point(1158, 148);
-            this.lblReservationLocationTotalAmountPrice.Name = "lblReservationLocationTotalAmountPrice";
-            this.lblReservationLocationTotalAmountPrice.Size = new System.Drawing.Size(25, 13);
-            this.lblReservationLocationTotalAmountPrice.TabIndex = 11;
-            this.lblReservationLocationTotalAmountPrice.Text = "650";
-            // 
             // ReservationLocation
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(1286, 859);
-            this.Controls.Add(this.lblReservationLocationTotalAmountPrice);
-            this.Controls.Add(this.lblReservationLocationTotalAmount);
-            this.Controls.Add(this.lblReservationLocationStillToDivideAmount);
-            this.Controls.Add(this.lblReservationLocationStillToDivide);
-            this.Controls.Add(this.lblReservationLocationAmountOfPeople);
-            this.Controls.Add(this.lblReservationLocationPersons);
+            this.ClientSize = new System.Drawing.Size(1715, 1045);
             this.Controls.Add(this.btnReservationLocationNext);
             this.Controls.Add(this.btnReservationLocationDivideGroup);
             this.Controls.Add(this.btnReservationLocationPrevious);
             this.Controls.Add(this.lblReservationLocationBungalow);
             this.Controls.Add(this.lblReservationLocationDetails);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ReservationLocation";
             this.Text = "ReservationLocation";
             this.ResumeLayout(false);
@@ -175,11 +115,5 @@
         private System.Windows.Forms.Button btnReservationLocationPrevious;
         private System.Windows.Forms.Button btnReservationLocationDivideGroup;
         private System.Windows.Forms.Button btnReservationLocationNext;
-        private System.Windows.Forms.Label lblReservationLocationPersons;
-        private System.Windows.Forms.Label lblReservationLocationAmountOfPeople;
-        private System.Windows.Forms.Label lblReservationLocationStillToDivide;
-        private System.Windows.Forms.Label lblReservationLocationStillToDivideAmount;
-        private System.Windows.Forms.Label lblReservationLocationTotalAmount;
-        private System.Windows.Forms.Label lblReservationLocationTotalAmountPrice;
     }
 }
